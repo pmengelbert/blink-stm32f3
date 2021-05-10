94587172a881ff0b6f573f0e08b1b655a4fbf380
Peter Engelbert <pmengelbert@gmail.com>
2021-05-10 15:23:56 -0500

    yes
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

---
+++ blink.s
@@ -0,0 +1,53 @@
+.syntax unified
+.thumb
+
+RCC_AHBENR = 0x40021014
+GPIOA_MODER = 0x48000000
+GPIOA_OTYPER = 0x48000004
+GPIOA_OSPEEDR = 0x48000008
+GPIOA_OPUPDR = 0x4800000c
+GPIOA_ODR = 0x48000014
+
+.global main
+.global TIM2_IRQHandler
+.text
+main:
+	@
+	ldr r1, =RCC_AHBENR
+	ldr r0, [r1]
+	orr r0, $0x00020000
+	str r0, [r1]
+
+	ldr r1, =GPIOA_MODER
+	ldr r0, [r1]
+	orr r0, $0x00000005
+	and r0, $0xfffffff5
+	str r0, [r1]
+
+	ldr r1, =GPIOA_OTYPER
+	ldr r0, [r1]
+	and r0, $0xfffffffc
+	str r0, [r1]
+
+	ldr r1, =GPIOA_OSPEEDR
+	ldr r0, [r1]
+	and r0, $0xfffffff0
+	str r0, [r1]
+
+	ldr r1, =GPIOA_OPUPDR
+	ldr r0, [r1]
+	and r0, $0xfffffff0
+	str r0, [r1]
+
+	ldr r1, =GPIOA_ODR
+	ldr r0, [r1]
+	orr r0, $0x00000001
+	and r0, $0xfffffffd
+	str r0, [r1]
+loop:
+	b loop
+
+TIM2_IRQHandler:
+	b TIM2_IRQHandler
+
+@ vim:ft=armv5
--- dma.s
+++ dma.s
@@ -1,7 +1,6 @@
 .syntax unified
 .thumb
 .global main
-.global daytuh
 
 RCC_AHBENR = 0x40021014
 
@@ -11,20 +10,14 @@ DMA_CNDTR1 = 0x4002000c
 DMA_CPAR1 = 0x40020010
 DMA_CMAR1 = 0x40020014
 
-DATA_SIZE = __data_size__
-
-LEN_STRING = 25
-
-
 .data
-daytuh:
 .asciz "poetic thoughts elude me"
 
 .text
 main:
 	ldr r0, =RCC_AHBENR
 	ldr r1, [r0]
-	orr r1, 0x00000001
+	orr r1, $0x00000001
 	str r1, [r0]
 
 	@ set up DMA
@@ -40,7 +33,7 @@ main:
 	ldr r1, [r0]
 	ldr r3, =$0xffff0000
 	and r1, r3
-	ldr r2, =DATA_SIZE @ I think in the linker script
+	ldr r2, =__data_size__
 	ldr r3, =$0x0000ffff
 	and r2, r3
 	orr r1, r2
@@ -50,9 +43,7 @@ main:
 	ldr r1, [r0]
 	ldr r3, =$0xffff8000
 	and r1, r3
-	ldr r2, =$0b111000011000000
-	ldr r3, =$0x00007fff
-	and r2, r3
+	ldr r2, =$(0b111000011000000 & 0x00007fff)
 	orr r1, r2
 	str r1, [r0]
 
@@ -65,3 +56,6 @@ main:
 	str r1, [r0]
 
 	b .
+
+
+@ vim:ft=armv5
